5e4018d1391119a91f5f5d2db3f974518016b36b
santhossunthar <santhoshsunthar@gmail.com>
2024-01-06 00:40:02 +0530

    Updated footer

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -1,4 +1,4 @@
 <div class="container-fluid d-flex justify-content-center 
     align-items-center bg-primary text-light p-3">
-    <h6>&copy; 2023 Your Website Name. All rights reserved.</h6>
+    <h6>&copy; 2023 Institute Inc. All rights reserved.</h6>
 </div>
