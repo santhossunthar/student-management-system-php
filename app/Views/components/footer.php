0f8dcd17a23a155637ce068fbe7d088e62ddc0f3
santhossunthar <santhoshsunthar@gmail.com>
2024-01-05 18:37:25 +0530

    Updated footer style

--- app/Views/components/footer.php
+++ app/Views/components/footer.php
@@ -1,1 +1,4 @@
-<p>&copy; 2023 Your Website Name. All rights reserved.</p>
+<div class="container-fluid d-flex justify-content-center 
+    align-items-center bg-primary text-light p-3">
+    <h6>&copy; 2023 Your Website Name. All rights reserved.</h6>
+</div>
