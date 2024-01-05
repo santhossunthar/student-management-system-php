0c475ebeb6ef120c2b71a3ebfdd61b1d22290000
santhossunthar <santhoshsunthar@gmail.com>
2024-01-06 01:27:23 +0530

    Added sidebar

---
+++ app/Views/components/dashboard/sidebar.php
@@ -0,0 +1,23 @@
+<div class="sidebar">
+    <div class="brand-logo">
+        <img src="<?php echo base_url('assets/images/brand-logo.jpg'); ?>" alt="logo">
+    </div>
+
+    <div class="list-group mt-4">
+        <a href="#" class="list-group-item list-group-item-action active">
+            Students
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            Lecturers
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            Courses
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            Attendances
+        </a>
+        <a href="#" class="list-group-item list-group-item-action">
+            Payments
+        </a>
+    </div>
+</div>
