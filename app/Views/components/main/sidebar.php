cdd722d1c00ac20c849282fa685edbd8b593b03a
santhossunthar <santhoshsunthar@gmail.com>
2024-01-07 01:26:54 +0530

    Added sidebar

---
+++ app/Views/components/main/sidebar.php
@@ -0,0 +1,27 @@
+<div class="sidebar">
+    <div class="brand-logo">
+        <img src="<?php echo base_url('assets/images/brand-logo.jpg'); ?>" alt="logo">
+    </div>
+
+    <div class="list-group text-center mt-4">
+        <a href="<?php echo base_url('dashboard'); ?>" 
+            class="list-group-item list-group-item-action sidebar-item-active active text-light">
+            Dashboard
+        </a>
+        <a href="<?php echo base_url('students'); ?>" class="list-group-item list-group-item-action sidebar-item text-light">
+            Students
+        </a>
+        <a href="<?php echo base_url('lecturers'); ?>" class="list-group-item list-group-item-action sidebar-item text-light">
+            Lecturers
+        </a>
+        <a href="<?php echo base_url('courses'); ?>" class="list-group-item list-group-item-action sidebar-item text-light">
+            Courses
+        </a>
+        <a href="<?php echo base_url('attendances'); ?>" class="list-group-item list-group-item-action sidebar-item text-light">
+            Attendances
+        </a>
+        <a href="<?php echo base_url('payments'); ?>" class="list-group-item list-group-item-action sidebar-item text-light">
+            Payments
+        </a>
+    </div>
+</div>
