df747bedaec85ed07a943873b6c1f6669998ba9f
santhossunthar <santhoshsunthar@gmail.com>
2024-01-07 01:26:07 +0530

    Updated routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,7 +11,17 @@ $routes->get('/register', 'AuthController::register');
 $routes->get('/logout', 'AuthController::logout');
 $routes->get('/dashboard', 'DashboardController::index');
 
-$routes->group('', ['namespace' => 'App\Controllers'], function ($routes) {
+$routes->get('/lecturers', 'DashboardController::index');
+$routes->get('/courses', 'DashboardController::index');
+$routes->get('/attendences', 'DashboardController::index');
+$routes->get('/payments', 'DashboardController::index');
+
+$routes->group('students', ['namespace' => 'App\Controllers'], function ($routes) {
+    $routes->get('', 'StudentController::getAll');
+    $routes->get('/get/(:any)', 'StudentController::get/$1');
+    $routes->post('/add', 'StudentController::add');
+    $routes->delete('/remove/(:any)', 'StudentController::delete/$1');
+    $routes->put('/update/(:any)', 'StudentController::update/$1');
 });
 
 # $routes->get('/hello/(:any)', 'HelloController::test/$1');
